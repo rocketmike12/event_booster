ea2f72a1e822d1bb5cb908fed928384254cff0df
rocketmike12 <mg.korop@gmail.com>
2025-05-25 20:29:36 +0300

    got sass to work properly; added reset, mixins and variables

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import path from "path"
+import { defineConfig } from "vite"
+import glob from "fast-glob"
+import { fileURLToPath } from "url"
+
+export default defineConfig({
+	build: {
+		minify: false, // disable minification
+		rollupOptions: {
+			input: Object.fromEntries(
+				glob
+					.sync(["./*.html", "./pages/**/*.html"])
+					.map((file) => [path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)), fileURLToPath(new URL(file, import.meta.url))])
+			),
+			// output unminified CSS file
+			output: {
+				assetFileNames: "assets/[name].[ext]",
+			},
+		},
+	},
+})
