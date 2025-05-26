3694a49e6b85672d32dd6a179a056e14116a652d
rocketmike12 <mg.korop@gmail.com>
2025-05-26 19:41:25 +0300

    added fetch and basic render. unfinished

---
+++ src/js/pagination.js
@@ -0,0 +1,25 @@
+let events;
+
+const render = function (data) {
+	data.forEach((el) => {
+		let newCard = document.createElement("li");
+		newCard.innerHTML = `
+			<svg class="event-list-icon"></svg>
+			<img class="event-list-img" src="${el.images[0].url}" alt="">
+			<h2 class="event-list-title">${el.name}</h2>
+			<p class="event-list-date">${el.dates.start.localDate}</p>
+			<p class="event-list-location"><svg class="event-list-location-icon"></svg>${el._embedded.venues[0].name}</p>
+		`;
+		newCard.className = "event-list-item";
+		document.body.appendChild(newCard);
+	});
+};
+
+async function getEvents() {
+	let res = await fetch("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=sES9o0k41AqBPlOAoQQCG4iYys2FN6TL");
+	res = await res.json();
+	events = res._embedded.events;
+	render(events);
+}
+
+getEvents();
